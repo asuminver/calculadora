0f186f4f3d0741ed10d87b25433c45af67bcb047
Ana Vicente <>
2013-04-11 11:51:33 +0200

    Cambios en la identación.

--- test/mi/calculadora/OperacionTest.java
+++ test/mi/calculadora/OperacionTest.java
@@ -9,9 +9,9 @@ import org.junit.Test;
  * @author Ana Vicente
  */
 public class OperacionTest {
-    
+
     Operacion o;
-    
+
     @Before
     public void prepare() {
         o = new Operacion();
@@ -19,8 +19,7 @@ public class OperacionTest {
     /* He creado un método preparación que se ejecutará antes de cada método de 
      * de prueba. 
      */
-    
-    
+
     @Test
     //0+5
     public void testsuma() {
@@ -30,10 +29,11 @@ public class OperacionTest {
 
         o.suma(n2);
         int resultado = o.obtenerAcumulado();
-        
-        assertEquals(n1+n2, resultado);
+
+        assertEquals(n1 + n2, resultado);
         //assertEquals(5, resultado);
     }
+
     @Test
     //0-7
     public void testresta() {
@@ -43,112 +43,112 @@ public class OperacionTest {
 
         o.resta(n2);
         int resultado = o.obtenerAcumulado();
-        
-        assertEquals(n1-n2, resultado);
+
+        assertEquals(n1 - n2, resultado);
         //assertEquals(7, resultado);
     }
-    
+
     @Test
     //18+32
     public void testsuma2() {
-    int n1 = 18;
-    int n2 = 32;
-    o.establecerAcumulado(n1);
-    
-    o.suma(n2);
-    int resultado = o.obtenerAcumulado();
-    
-    assertEquals(n1+n2, resultado);
+        int n1 = 18;
+        int n2 = 32;
+        o.establecerAcumulado(n1);
+
+        o.suma(n2);
+        int resultado = o.obtenerAcumulado();
+
+        assertEquals(n1 + n2, resultado);
     }
-    
+
     @Test
     //27-37
     public void testresta2() {
-    int n1 = 27;
-    int n2 = 37;
-    o.establecerAcumulado(n1);
-    
-    o.resta(n2);
-    int resultado = o.obtenerAcumulado();
-    
-    assertEquals(n1-n2, resultado);            
+        int n1 = 27;
+        int n2 = 37;
+        o.establecerAcumulado(n1);
+
+        o.resta(n2);
+        int resultado = o.obtenerAcumulado();
+
+        assertEquals(n1 - n2, resultado);
     }
-    
+
     @Test
     //0*100
-    public void testmultiplica(){
-    int n1 = 0;
-    int n2 = 100;
-    o.establecerAcumulado(n1);
-    
-    o.multiplica(n2);
-    int resultado = o.obtenerAcumulado();
-    
-    assertEquals(n1*n2, resultado);
+    public void testmultiplica() {
+        int n1 = 0;
+        int n2 = 100;
+        o.establecerAcumulado(n1);
+
+        o.multiplica(n2);
+        int resultado = o.obtenerAcumulado();
+
+        assertEquals(n1 * n2, resultado);
     }
-    
+
     @Test
     //0/2
-    public void testdivide(){
-    int n1 = 0;
-    int n2 = 2;
-    o.establecerAcumulado(n1);
-    
-    o.divide(n2);
-    int resultado = o.obtenerAcumulado();
-    
-    assertEquals(n1/n2, resultado);
+    public void testdivide() {
+        int n1 = 0;
+        int n2 = 2;
+        o.establecerAcumulado(n1);
+
+        o.divide(n2);
+        int resultado = o.obtenerAcumulado();
+
+        assertEquals(n1 / n2, resultado);
     }
-   
+
     @Test
     //20/0
-    public void testdivide2(){
-    int n1 = 20;
-    int n2 = 0;
-    o.establecerAcumulado(n1);
-    
-    o.divide(n2);
-    int resultado = o.obtenerAcumulado();
-    
-    assertEquals(n1/n2, resultado);
+    public void testdivide2() {
+        int n1 = 20;
+        int n2 = 0;
+        o.establecerAcumulado(n1);
+
+        o.divide(n2);
+        int resultado = o.obtenerAcumulado();
+
+        assertEquals(n1 / n2, resultado);
     }
-    
+
     @Test
     //40/2
-    public void testdivide3(){
-    int n1 = 40;
-    int n2 = 2;
-    o.establecerAcumulado(n1);
-    
-    o.divide(n2);
-    int resultado = o.obtenerAcumulado();
-    
-    assertEquals(n1/n2, resultado);
+    public void testdivide3() {
+        int n1 = 40;
+        int n2 = 2;
+        o.establecerAcumulado(n1);
+
+        o.divide(n2);
+        int resultado = o.obtenerAcumulado();
+
+        assertEquals(n1 / n2, resultado);
     }
-    
+
     @Test
     //333*20
-    public void testmultiplica2(){
-    int n1 = 333;
-    int n2 = 20;
-    o.establecerAcumulado(n1);
-    
-    o.multiplica(n2);
-    int resultado = o.obtenerAcumulado();
-    
-    assertEquals(n1*n2, resultado);
+    public void testmultiplica2() {
+        int n1 = 333;
+        int n2 = 20;
+        o.establecerAcumulado(n1);
+
+        o.multiplica(n2);
+        int resultado = o.obtenerAcumulado();
+
+        assertEquals(n1 * n2, resultado);
     }
-    
+
     @Test
     //0-0
-    public void testresta3(){
-    int n1 = 0;
-    int n2 = 0;
-    o.establecerAcumulado(n1);
-    
-    o.resta(n2);
-    int resultado = o.obtenerAcumulado();
-    
-    assertEquals(n1-n2, resultado);  
+    public void testresta3() {
+        int n1 = 0;
+        int n2 = 0;
+        o.establecerAcumulado(n1);
+
+        o.resta(n2);
+        int resultado = o.obtenerAcumulado();
+
+        assertEquals(n1 - n2, resultado);
     }
 }
