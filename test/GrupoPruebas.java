8b27b29560d816d9176994137bf160dfb4a6fce9
Administrador <Administrador@192.168.190.128>
2013-05-21 16:51:46 +0200

    Test de la calculadora

---
+++ test/GrupoPruebas.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author Administrador
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({})
+public class GrupoPruebas {
+    
+}
